b712cf3af96403b7639a4eeaeaa97696c0e7ee63
Abhishek Chand <abhishekc117@hotmail.com>
2025-02-16 15:23:49 +1100

    removed submitmessage component, created test component for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import { SubmitMessage } from './components/SubmitMessage'
+import { Test } from './components/Test'
 
 function App() {
   const [message, setMessage] = useState('')
 
   const submitHandler = (e) => {
     e.preventDefault();
-    SubmitMessage(message);
+
+    fetch('http://localhost:3000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({userMessage: message})
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+
     e.target.reset();
   }
 
   return (
     <>
-    <form onSubmit={submitHandler} className="input-container">
-      <div className="input-field" onChange={(e) => setMessage(e.target.value)} contentEditable="true"></div>
-      <button className="input-button" type='submit'>Go</button>
+    <div className='chat-container'>
+      <Test></Test>
+    </div>
+    <form onSubmit={submitHandler} className='input-container'>
+      <div className='input-field' onChange={(e) => setMessage(e.target.value)} contentEditable='true'></div>
+      <button className='input-button' type='submit'>Go</button>
     </form>
     </>
   )
---
+++ client/src/components/Test.jsx
@@ -0,0 +1,5 @@
+export function Test() {
+    return (
+        <p>Hello World!</p>
+    )
+}
